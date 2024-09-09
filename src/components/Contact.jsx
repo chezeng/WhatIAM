9fd7c481871772e6567f74e66d2cbb781e916b95
Cheng Z <110141788+ch3ngZ@users.noreply.github.com>
2024-09-08 22:55:17 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
         Feel free to contact me and I will respond <span className='gradient bg-gradient-to-r from-blue-500 to-blue-700 bg-clip-text text-transparent font-bold inline font-bold'>as soon as possible</span>. <br></br>
       </h2>   
       
-{/*       <div className="bg-gray-900 bg-opacity-80 backdrop-filter backdrop-blur-lg p-8 rounded-lg shadow-lg mt-10 w-full">
+      <div className="bg-gray-900 bg-opacity-80 backdrop-filter backdrop-blur-lg p-8 rounded-lg shadow-lg mt-10 w-full">
         <h1 className="text-3xl font-bold paragraph text-white mb-8">Let's Craft Something Extraordinary!</h1>
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
@@ -142,14 +142,6 @@ const handleSubmit = async (e) => {
             {successMessage}
           </div>
         )}
-      </div> */}
-
-      <div className='card-wrapper-button font-bold h-[5rem] w-[15rem] hover:scale-105 transition ease-in-out -mt-10'>
-          <a href="mailto:virtualstar0125@gmail.com" target="_blank" rel="noopener noreferrer">
-            <div className='card-content-button rounded-3xl cursor-pointer'>
-              <p className="text-center text-2xl font-bold mt-5">Contact Me</p>
-            </div>
-          </a>
       </div>
 
       <div className='absolute bottom-1/2 left-0 w-full flex justify-center opacity-10 pointer-events-none'>
