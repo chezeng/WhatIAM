60e387fcbc9893d6281bda5c1428f32cc30e17f1
VirtualStar <VirtualStar0125@gmail.com>
2024-09-29 19:58:40 -0400

    A huge update

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import NavBar from './HomeComponents/NavBar';
+import Intro from './HomeComponents/Intro';
+import About from './HomeComponents/About';
+import Experience from './HomeComponents/Experience';
+import Projects from './HomeComponents/Projects';
+import Contact from './HomeComponents/Contact';
+import Footer from './HomeComponents/Footer';
+
+const App = () => {
+  return (
+    <div className="bg-black">
+      <NavBar></NavBar>
+      <Intro></Intro>
+      <About></About>
+      <Projects></Projects>
+      <Experience></Experience>
+      <Contact></Contact>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App;
