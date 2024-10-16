f03ea262a8ff84ae50f4e3ba16e4cb482a827201
ch3ngZ <VirtualStar0125@gmail.com>
2024-10-16 17:00:15 -0400

    env place

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import rateLimit from 'express-rate-limit';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 const app = express();
 const PORT = 3000; 
