17c6fae8c051fc02cfe7d6ffead2306f48fb93f6
ch3ngZ <VirtualStar0125@gmail.com>
2024-10-19 12:01:30 -0400

    test

--- client-blog/src/components/ArticlePage.jsx
+++ client-blog/src/components/ArticlePage.jsx
@@ -3,9 +3,10 @@ import { useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 
 function ArticlePage() {
-  const { id } = useParams();
-  const [article, setArticle] = useState(null);
-  const [articleContent, setArticleContent] = useState('');
+  const { id } = useParams(); 
+  const [article, setArticle] = useState(null);  
+  const [articleContent, setArticleContent] = useState(''); 
+
   const labelColors = {
     "Software": "bg-blue-500",
     "Audio-Visual": "bg-red-500",
@@ -16,35 +17,37 @@ function ArticlePage() {
   };
 
   useEffect(() => {
-    fetch(`https://server.chengzeng.dev/api/articles/${id}`)
+    fetch(`https://chezeng.github.io/Media/WhatIAM/articles/articles.json`)
       .then(response => response.json())
       .then(data => {
-        setArticle(data);
+        const foundArticle = data.find(article => article.id === parseInt(id));
+        if (foundArticle) {
+          setArticle(foundArticle);  
+        }
       })
-      .catch(error => console.error('Error fetching article:', error));
+      .catch(error => console.error('Error fetching article metadata:', error));
 
     fetch(`https://chezeng.github.io/Media/WhatIAM/articles/${id}.md`)
       .then(response => response.text())
-      .then(data => setArticleContent(data))
+      .then(data => setArticleContent(data)) 
       .catch(error => console.error('Error fetching article content:', error));
   }, [id]);
 
   if (!article) return <div>Loading...</div>;
 
   return (
-    <div className="px-20 md:px-48 xl:px-72 pt-32">
+    <div className="px-20 md:px-48 xl:px-72 pt-20">
       <div className="flex space-x-2 mt-2 mb-4">
-        {article.labels.map((label, index) => (
+        {article.labels && article.labels.map((label, index) => (
           <span key={index} 
             className={`text-white text-sm px-2 py-1 rounded-full cursor-pointer ${labelColors[label]}`}>
             {label}
           </span>
         ))}
       </div>
-      <h1 className="text-4xl font-bold mb-4">{article.title}</h1>
-      <p className="text-gray-500 mb-8">{article.date}</p>
-      <img src={article.image} alt={article.title} className="mb-8 w-full h-auto" />
-      <ReactMarkdown>{articleContent}</ReactMarkdown>
+      
+      <ReactMarkdown className='markdown'>{articleContent}</ReactMarkdown>
+
     </div>
   );
 }
