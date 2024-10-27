aab492ac9bbb129fa9ab2a3dfb75f164b3d4bbe4
Cheng Zeng <VirtualStar0125@gmail.com>
2024-10-27 19:23:45 -0400

    Update AboutPage.jsx

--- client-blog/src/pages/AboutPage.jsx
+++ client-blog/src/pages/AboutPage.jsx
@@ -3,10 +3,10 @@ function AboutPage({ theme }) {
     <div id="top" className="h-svh grid grid-rows-2 space-y-28 lg:space-y-0 sm:grid-cols-2 px-20 items-center pt-52 pb-0 iphone:pb-20 sm:pb-0 lg:pt-52" style={{ background: `linear-gradient(to bottom, ${theme.from}, ${theme.to})`}}>
         <div className='space-y-10'>
           <ul className="list-disc space-y-5 text-left text-xl md:text-3xl text-black font-bold italic">
-            <li>I am always learning <span className="bg-yellow-300 p-2">Computer Science</span>.</li>
-            <li>I aspire to fully utilize <span className="bg-yellow-300 p-2">AI + software applications</span> to create impact.</li>
-            <li>I have a personal website at <span className="bg-yellow-300 p-2"><a href="https://www.chengzeng.dev" target="_blank" rel="noopener noreferrer" className="text-blue-500">chengzeng.dev</a></span>.</li>
-            <li>You can refresh the webpage to <span className="bg-yellow-300 p-2">change the theme</span>!</li>
+            <li>I am always learning <span className="bg-yellow-300 px-1">Computer Science</span>.</li>
+            <li>I aspire to fully utilize <span className="bg-yellow-300 px-1">AI + software applications</span> to create impact.</li>
+            <li>I have a personal website at <span className="bg-yellow-300 px-1"><a href="https://www.chengzeng.dev" target="_blank" rel="noopener noreferrer" className="text-blue-500">chengzeng.dev</a></span>.</li>
+            <li>You can refresh the webpage to <span className="bg-yellow-300 px-1">change the theme</span>!</li>
           </ul>
         </div>
 
